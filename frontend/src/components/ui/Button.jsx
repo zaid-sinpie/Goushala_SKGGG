12f2b930b3097db8aa9465c5a1fe04a3d055ccdc
zaid-sinpie <zaidmasuldar@gmail.com>
2025-12-14 13:16:20 +0530

    fixed button text

--- frontend/src/components/ui/Button.jsx
+++ frontend/src/components/ui/Button.jsx
@@ -4,7 +4,7 @@ const Button = ({ children, ...props }) => {
   return (
     <button
       {...props}
-      className="btn relative inline-flex items-center justify-start overflow-hidden font-thin tracking-widest transition-all bg-dark rounded-sm hover:bg-dullText group py-2 px-5 text-sm shadow-md"
+      className="btn relative inline-flex items-center justify-start overflow-hidden font-thin tracking-widest transition-all bg-dark rounded-sm hover:bg-dullText group py-2 px-5 text-[12px] shadow-md"
     >
       <span className="w-56 h-48 rounded bg-text absolute bottom-0 left-0 translate-x-full ease-out duration-500 transition-all translate-y-full mb-9 ml-9 group-hover:ml-0 group-hover:mb-32 group-hover:translate-x-0"></span>
       <span className="relative w-full text-left text-text transition-colors duration-300 ease-in-out group-hover:text-dark">
